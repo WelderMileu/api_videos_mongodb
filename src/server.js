0e16845054467bcb167ece19bf5eadebd08ad4fb
weldermileu <welderfelix21@gmail.com>
2020-08-14 13:22:56 -0300

    adicionando o cors a nossa aplicação

--- src/server.js
+++ src/server.js
@@ -1,15 +1,16 @@
-const express = require('express')
-const app = express()
-const config = require('dotenv').config() 
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const config = require('dotenv').config(); 
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const connection = require('./config/connect.js')
 const collection_video = require('./collections/collection_video.js')
 const router = require('./routes.js')
 
+app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-
 app.use(router)
 
 const port = process.env.PORT || 3000
